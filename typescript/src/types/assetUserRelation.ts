31b7a8846f784cf5dfd0710b1d92163f03b0d97e
sebastian karlsson <sebbekarlsson97@gmail.com>
2023-09-08 18:26:52 +0200

    remove pendingInvestorId

--- typescript/src/types/assetUserRelation.ts
+++ typescript/src/types/assetUserRelation.ts
@@ -2,7 +2,6 @@ import { EAssetUserRole } from "./assetUserRole";
 
 export interface IAssetUserRelation {
   userId?: string;
-  pendingInvestorId?: string;
   assetId: string;
   role: EAssetUserRole;
 }
