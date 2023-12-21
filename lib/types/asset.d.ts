55730e965882fb62255c703924dcb3d87ec95613
sebastian karlsson <sebbekarlsson97@gmail.com>
2023-12-21 16:17:44 +0100

    adds asset.type

--- lib/types/asset.d.ts
+++ lib/types/asset.d.ts
@@ -1,11 +1,18 @@
 import * as ss from 'superstruct';
 import { ILedger } from ".";
+export declare enum EAssetType {
+    LISTED_EQUITY = "LISTED_EQUITY",
+    UNLISTED_EQUITY = "UNLISTED_EQUITY",
+    REAL_ESTATE = "REAL_ESTATE",
+    ALTERNATIVE = "ALTERNATIVE"
+}
 export interface IAsset {
     name: string;
     organizationNumber: string;
     contactEmail: string;
     ledger: ILedger;
     assetId?: string;
+    type?: EAssetType;
 }
 export declare const AssetSchema: ss.Describe<IAsset>;
 export type ICompany = IAsset;
--- src/types/asset.ts
+++ src/types/asset.ts
@@ -1,6 +1,12 @@
 import * as ss from 'superstruct';
 import { ILedger, LedgerSchema } from ".";
 
+export enum EAssetType {
+  LISTED_EQUITY = "LISTED_EQUITY",
+  UNLISTED_EQUITY = "UNLISTED_EQUITY",
+  REAL_ESTATE = "REAL_ESTATE",
+  ALTERNATIVE = "ALTERNATIVE"
+}
 
 export interface IAsset {
   name: string;
@@ -8,6 +14,7 @@ export interface IAsset {
   contactEmail: string;
   ledger: ILedger;
   assetId?: string;
+  type?: EAssetType;
 }
 
 export const AssetSchema: ss.Describe<IAsset> = ss.type({
@@ -16,6 +23,7 @@ export const AssetSchema: ss.Describe<IAsset> = ss.type({
   contactEmail: ss.string(),
   ledger: LedgerSchema,
   assetId: ss.optional(ss.any()),
+  type: ss.optional(ss.enums([EAssetType.LISTED_EQUITY, EAssetType.UNLISTED_EQUITY, EAssetType.REAL_ESTATE, EAssetType.ALTERNATIVE]))
 });
 
 
