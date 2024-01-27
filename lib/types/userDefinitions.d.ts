75c0c1d97fbb08180de288e7488a7612500f9c9e
sebastian karlsson <sebbekarlsson97@gmail.com>
2024-01-27 20:36:47 +0100

    adds userId to user definitions

--- lib/types/userDefinitions.d.ts
+++ lib/types/userDefinitions.d.ts
@@ -2,6 +2,7 @@ import { IAssetRelation } from './assetRelation';
 import { EColorPreference } from './colorPreference';
 import { ECurrency } from './currency';
 export interface IUserDefinitions {
+    userId: string;
     assetRelations: IAssetRelation;
     colorPreference?: EColorPreference;
     currency?: ECurrency;
--- src/types/userDefinitions.ts
+++ src/types/userDefinitions.ts
@@ -3,6 +3,7 @@ import { EColorPreference } from './colorPreference';
 import { ECurrency } from './currency';
 
 export interface IUserDefinitions {
+  userId: string;
   assetRelations: IAssetRelation;
   colorPreference?: EColorPreference;
   currency?: ECurrency;
