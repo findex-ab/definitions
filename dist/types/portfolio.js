911999b32c2b359f0e8226db71496c433cb5cb81
sebastian karlsson <sebbekarlsson97@gmail.com>
2024-02-20 11:30:50 +0100

    bump version & remove performance array

--- dist/types/portfolio.js
+++ dist/types/portfolio.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PortfolioSchema = exports.PortfolioValueSlotSchema = void 0;
 const asset_1 = require("./asset");
 const ss = require("superstruct");
-const performance_1 = require("./performance");
 exports.PortfolioValueSlotSchema = ss.type({
     assetCount: ss.number(),
     value: ss.number(),
@@ -19,7 +18,6 @@ exports.PortfolioSchema = ss.type({
         asset_1.EAssetType.REAL_ESTATE,
         asset_1.EAssetType.UNDEFINED,
         asset_1.EAssetType.UNLISTED_EQUITY,
-    ]), exports.PortfolioValueSlotSchema),
-    performance: ss.optional(ss.array(performance_1.PerformanceSchema))
+    ]), exports.PortfolioValueSlotSchema)
 });
 //# sourceMappingURL=portfolio.js.map
