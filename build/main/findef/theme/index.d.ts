6ed3d7870910ff48af4ec7c9b01299aa144d80cb
sebastian karlsson <sebbekarlsson97@gmail.com>
2024-06-28 22:12:36 +0200

    add theme

---
+++ build/main/findef/theme/index.d.ts
@@ -0,0 +1,35 @@
+export declare const theme: {
+    colors: {
+        primary: {
+            5: string;
+            10: string;
+            30: string;
+            50: string;
+            90: string;
+            100: string;
+            200: string;
+            400: string;
+            500: string;
+            700: string;
+            800: string;
+            900: string;
+            950: string;
+        };
+        text: {
+            50: string;
+        };
+        grayish: {
+            100: string;
+        };
+        findex: {
+            100: string;
+            50: string;
+            500: string;
+            800: string;
+            900: string;
+        };
+        error: {
+            100: string;
+        };
+    };
+};
--- build/module/findef/index.js
+++ build/module/findef/index.js
@@ -1,3 +1,4 @@
+export * from './theme';
 export * from './dbModel';
 export * from './session';
 export * from './pagination';
