8cf7dabcff43c4c864f0a7b6f2c9773c090bf647
sebastian karlsson <sebbekarlsson97@gmail.com>
2024-08-03 02:07:09 +0200

    more app events

--- build/main/findef/applicationEvent.d.ts
+++ build/main/findef/applicationEvent.d.ts
@@ -1,12 +1,14 @@
 import { TDocRef } from "./docref";
 import { IUser } from "./user";
 export declare enum EApplicationEventType {
+    CREATE_USER_ACCOUNT = "CREATE_USER_ACCOUNT",
     CREATE_INVESTMENT = "CREATE_INVESTMENT",
     CREATE_ASSET = "CREATE_ASSET",
     EDIT_INVESTMENT = "EDIT_INVESTMENT",
     EDIT_ASSET = "EDIT_ASSET",
     DELETE_INVESTMENT = "DELETE_INVESTMENT",
     DELETE_ASSET = "DELETE_ASSET",
+    DELETE_ATTACHMENT = "DELETE_ATTACHMENT",
     PROVIDER_IMPORT = "PROVIDER_IMPORT",
     RESOLVE_INVITE = "RESOLVE_INVITE",
     INVITE_ALREADY_RESOLVED = "INVITE_ALREADY_RESOLVED",
@@ -19,6 +21,8 @@ export type IApplicationEvent<T extends Dict = Dict> = {
     triggeredBy: TDocRef<IUser>;
     type: EApplicationEventType;
     tags: string[];
+    processed?: boolean;
+    processedAt?: Date;
     payload: T;
 };
 export {};
--- build/main/findef/applicationEvent.js
+++ build/main/findef/applicationEvent.js
@@ -3,12 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.EApplicationEventType = void 0;
 var EApplicationEventType;
 (function (EApplicationEventType) {
+    EApplicationEventType["CREATE_USER_ACCOUNT"] = "CREATE_USER_ACCOUNT";
     EApplicationEventType["CREATE_INVESTMENT"] = "CREATE_INVESTMENT";
     EApplicationEventType["CREATE_ASSET"] = "CREATE_ASSET";
     EApplicationEventType["EDIT_INVESTMENT"] = "EDIT_INVESTMENT";
     EApplicationEventType["EDIT_ASSET"] = "EDIT_ASSET";
     EApplicationEventType["DELETE_INVESTMENT"] = "DELETE_INVESTMENT";
     EApplicationEventType["DELETE_ASSET"] = "DELETE_ASSET";
+    EApplicationEventType["DELETE_ATTACHMENT"] = "DELETE_ATTACHMENT";
     EApplicationEventType["PROVIDER_IMPORT"] = "PROVIDER_IMPORT";
     EApplicationEventType["RESOLVE_INVITE"] = "RESOLVE_INVITE";
     EApplicationEventType["INVITE_ALREADY_RESOLVED"] = "INVITE_ALREADY_RESOLVED";
