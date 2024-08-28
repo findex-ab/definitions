ea564755b589990b2f1a538a892c3c56108de2b7
sebastian karlsson <sebbekarlsson97@gmail.com>
2024-08-28 10:11:50 +0200

    createdBy

--- build/main/findef/referral.d.ts
+++ build/main/findef/referral.d.ts
@@ -1,5 +1,8 @@
+import { TDocRef } from "./docref";
+import { IUser } from "./user";
 export type IReferral = {
     firstname: string;
     lastname: string;
     email: string;
+    createdBy: TDocRef<IUser>;
 };
