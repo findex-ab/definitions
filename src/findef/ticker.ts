272692047be00dc6bee1259e3c9e64e8fd2e291e
Filip Larsson <filip.larsson.vbg@gmail.com>
2024-09-04 14:52:51 +0200

    updated ticker definition

--- src/findef/ticker.ts
+++ src/findef/ticker.ts
@@ -9,6 +9,38 @@ export type ITicker = {
   stockExchange: string;
   exchangeShortName: string;
   image?: TDocRef<IAttachment>;
+  price: Number;
+  beta: Number;
+  volAvg: Number;
+  mktCap: Number;
+  lastDiv: Number;
+  range: string;
+  changes: Number;
+  companyName: string;
+  cik: string;
+  isin: string;
+  cusip: string;
+  exchange: string;
+  industry: string;
+  website: string;
+  description: string;
+  ceo: string,
+  sector: string,
+  country: string,
+  fullTimeEmployees: Number,
+  phone: string,
+  address: string,
+  city: string,
+  state: string,
+  zip: string,
+  dcfDiff: Number,
+  dcf: Number,
+  ipoDate: String,
+  defaultImage: Boolean,
+  isEtf: Boolean,
+  isActivelyTrading: Boolean,
+  isAdr: Boolean,
+  isFund: Boolean
 }
 
 export type IFindexTicker = ITicker & ITickerQuote;
