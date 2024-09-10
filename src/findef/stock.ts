35b48488c109beb05c6ac0ac2f56f656a3261e10
sebastian karlsson <sebbekarlsson97@gmail.com>
2024-09-10 12:31:50 +0200

    extend stock type

--- src/findef/stock.ts
+++ src/findef/stock.ts
@@ -1,7 +1,15 @@
+import { IAttachment } from "./attachment";
+import { TDocRef } from "./docref";
+
 export type IStock = {
   symbol: string;
   name: string;
   change: number;
   price: number;
   changesPercentage: number;
+  exchange: string;
+  exchangeShortName: string;
+  stockType: string;
+  searchPrio: number;
+  image?: TDocRef<IAttachment>;
 };
