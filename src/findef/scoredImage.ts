6422a678f86aba29fc5dc404e3edfe1681135229
sebastian karlsson <sebbekarlsson97@gmail.com>
2025-04-02 15:25:08 +0200

    scored image

---
+++ src/findef/scoredImage.ts
@@ -0,0 +1,12 @@
+import { IAttachment } from "./attachment";
+import { TDocRef } from "./docref";
+
+export type IScoredImage =  {
+  domain: string;
+  score: number;
+  src: string;
+  query: string;
+  failed?: boolean;
+  attachment?: TDocRef<IAttachment>;
+  name?: string;
+}
