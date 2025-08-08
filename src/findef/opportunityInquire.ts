0d8d7a55bf3aad31532144d6d1854c29e247d03a
Filip Larsson <filip.larsson.vbg@gmail.com>
2025-08-08 08:15:36 +0200

    update opportunityInquire

--- src/findef/opportunityInquire.ts
+++ src/findef/opportunityInquire.ts
@@ -13,6 +13,7 @@ export type IOpportunityInquire = {
   user: TDocRef<IUser>;
   inquireType: EOpportunityInquireType;
   note?: string;
-  amount?: IValue;
+  sharePrice?: IValue;
+  quantity?: number;
   opportunity: TDocRef<IOpportunity>;
 }
