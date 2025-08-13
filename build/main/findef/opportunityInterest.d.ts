1e3e1cdacb7a1fdb7793c657d42f69a2cdaca2e0
Filip Larsson <filip.larsson.vbg@gmail.com>
2025-08-13 13:45:36 +0200

    add assettype listed and assetsubtype to opportunity interes

--- build/main/findef/opportunityInterest.d.ts
+++ build/main/findef/opportunityInterest.d.ts
@@ -1,3 +1,4 @@
+import { EAssetSubtype, EAssetType } from "./asset";
 import { TDocRef } from "./docref";
 import { EOpportunityType } from "./opportunityType";
 import { IUser } from "./user";
@@ -5,4 +6,7 @@ export type IOpportunityInterest = {
     user: TDocRef<IUser>;
     note?: string;
     opportunityType: EOpportunityType;
+    assetType?: EAssetType;
+    listed?: boolean;
+    assetSubtype?: EAssetSubtype;
 };
--- src/findef/opportunityInterest.ts
+++ src/findef/opportunityInterest.ts
@@ -1,3 +1,4 @@
+import { EAssetSubtype, EAssetType } from "./asset";
 import { TDocRef } from "./docref";
 import { EOpportunityType } from "./opportunityType";
 import { IUser } from "./user";
@@ -6,4 +7,7 @@ export type IOpportunityInterest = {
   user: TDocRef<IUser>;
   note?: string;
   opportunityType: EOpportunityType;
+  assetType?: EAssetType;
+  listed?: boolean;
+  assetSubtype?: EAssetSubtype
 }
