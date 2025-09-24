31e9445756221b2c26a9cedabf1fd8f3f2b0fbfc
sebastian karlsson <sebbekarlsson97@gmail.com>
2025-09-24 11:32:39 +0200

    input guardrails

--- src/findef/ai/agent.ts
+++ src/findef/ai/agent.ts
@@ -11,5 +11,6 @@ export type IAIAgent<OutputType extends Record<string, any> = Record<string, any
   agentType: EAIAgentType;
   identifier: string;
   outputGuardrailIdentifiers?: Array<string>;
+  inputGuardrailIdentifiers?: Array<string>;
   outputType?: OutputType;
 }
