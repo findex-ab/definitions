ab52237384c217366a76bd1c5a3a9c918abf835a
sebastian karlsson <sebbekarlsson97@gmail.com>
2025-09-24 14:36:12 +0200

    add description to agent

--- build/module/findef/ai/agent.d.ts
+++ build/module/findef/ai/agent.d.ts
@@ -9,6 +9,7 @@ export type IAIAgent<OutputType extends Record<string, any> = Record<string, any
     instructions: string;
     agentType: EAIAgentType;
     identifier: string;
+    description?: string;
     outputGuardrailIdentifiers?: Array<string>;
     inputGuardrailIdentifiers?: Array<string>;
     outputType?: OutputType;
