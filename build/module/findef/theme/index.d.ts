33ab8088352f0cbc1c4a16edb0227b8c62049c5e
Filip Larsson <filip.larsson.vbg@gmail.com>
2026-01-26 11:46:25 +0100

    new color palette

--- build/module/findef/theme/index.d.ts
+++ build/module/findef/theme/index.d.ts
@@ -1,30 +1,26 @@
 export declare const theme: {
     colors: {
         primary: {
-            5: string;
-            10: string;
-            30: string;
             50: string;
-            90: string;
             100: string;
             200: string;
+            300: string;
             400: string;
             500: string;
+            600: string;
             700: string;
             800: string;
             900: string;
-            950: string;
-        };
-        text: {
-            50: string;
-        };
-        grayish: {
-            100: string;
         };
         findex: {
-            100: string;
             50: string;
+            100: string;
+            200: string;
+            300: string;
+            400: string;
             500: string;
+            600: string;
+            700: string;
             800: string;
             900: string;
         };
